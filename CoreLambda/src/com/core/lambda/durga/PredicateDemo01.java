148e393f6b10577a8f28872475595c12dfc56b04
Sudhakar <Sudhakar23>
2020-12-07 17:55:23 +0530

    PredicateDemo01 added.

---
+++ CoreLambda/src/com/core/lambda/durga/PredicateDemo01.java
@@ -0,0 +1,17 @@
+package com.core.lambda.durga;
+
+import java.util.function.Predicate;
+
+public class PredicateDemo01 implements Predicate<Integer> {
+
+	@Override
+	public boolean test(Integer t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public static void main(String[] args) {
+		
+	}
+
+}
