e5e13ba3760bf3ed8d36967b1dcbb0163c8ed4a7
Sudhakar <Sudhakar23>
2020-12-08 11:18:51 +0530

    PredicateDemo01 modified.

--- CoreLambda/src/com/core/lambda/durga/PredicateDemo01.java
+++ CoreLambda/src/com/core/lambda/durga/PredicateDemo01.java
@@ -2,16 +2,24 @@ package com.core.lambda.durga;
 
 import java.util.function.Predicate;
 
-public class PredicateDemo01 implements Predicate<Integer> {
-
-	@Override
-	public boolean test(Integer t) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+public class PredicateDemo01{
 
 	public static void main(String[] args) {
-		
+		Predicate<Integer> p = (t) -> { return t%2==0 ? true : false;};
+		System.out.println(p.test(4));
 	}
-
 }
+
+// Without using Lambda expression
+
+//public class PredicateDemo01 implements Predicate<Integer>{
+//	@Override
+//	public boolean test(Integer t) {
+//		return t%2==0 ? true : false;
+//	}
+//	
+//	public static void main(String[] args) {
+//		PredicateDemo01 pd = new PredicateDemo01();
+//		System.out.println(pd.test(3));
+//	}
+//}
