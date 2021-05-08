d3ac85431270059468198f3bf32b1542c2554ab0
Sudhakar <Sudhakar23>
2021-05-08 20:54:50 +0530

    File rename to video No

--- CoreLambda/src/com/core/lambda/durga/FunInterEx1_08.java
+++ CoreLambda/src/com/core/lambda/durga/FunInterEx1_08.java
@@ -1,13 +1,26 @@
 package com.core.lambda.durga;
 
+//@FunctionalInterface
 interface Interf {
 	public void add(int a, int b);
 	public default void mul(int a, int b) {};
 }
-public class FunInterEx1{
+public class FunInterEx1_08{
 	public static void main(String[] args) {
 		Interf interf = (a, b)-> System.out.println("Sum :" +(a+b));
 		interf.add(12, 34);
 		interf.add(12, 5);
+		
+		Interf interf2 = new Interf() {
+			@Override
+			public void add(int a, int b) {
+				System.out.println("Sum :" + (a+b));
+				
+			}
+			public void mul() {
+				
+			}
+		};
+		interf2.add(22, 33);
 	}
 }
