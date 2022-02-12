f41c999de0a7c4cb98317d7932dba946e5565f6e
Sudhakar23 <sudhakar.prsd23@gmail.com>
2022-02-12 18:04:59 +0530

    EBillCalculateAVK file added

---
+++ corejava-interview-question/src/com/java/myprog/EBillCalculateAVK.java
@@ -0,0 +1,14 @@
+package com.java.myprog;
+
+import java.util.Scanner;
+
+public class EBillCalculateAVK {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int nextInt = scanner.nextInt();
+		
+		
+	}
+
+}
