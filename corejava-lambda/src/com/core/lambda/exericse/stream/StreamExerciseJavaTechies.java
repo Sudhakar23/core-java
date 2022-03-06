9b7ba80f7ac5f9a6e55335b56de3419502633c6c
Sudhakar23 <sudhakar.prsd23@gmail.com>
2022-03-06 23:11:47 +0530

    Stream files modified

--- corejava-lambda/src/com/core/lambda/exericse/stream/StreamExerciseJavaTechies.java
+++ corejava-lambda/src/com/core/lambda/exericse/stream/StreamExerciseJavaTechies.java
@@ -2,9 +2,12 @@ package com.core.lambda.exericse.stream;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -12,18 +15,18 @@ public class StreamExerciseJavaTechies {
 
 	public static void main(String[] args) {
 		List<Employee> employees = Arrays.asList(
-				new Employee("001", "Nitish", 	25, "Male", 	"HR", 					"24000", "active", 2014),
-				new Employee("002", "Amit", 	25, "Male", 	"Sales & Mkt",			"20000", "active", 2014),
+				new Employee("001", "Nitish", 	29, "Male", 	"HR", 					"24000", "active", 2014),
+				new Employee("002", "Amit", 	29, "Male", 	"Sales & Mkt",			"20000", "active", 2014),
 				new Employee("003", "Diya", 	22, "Female", 	"HR", 					"14000", "active", 2017),
 				new Employee("005", "Venkat", 	20, "Male", 	"Product Development", 	"25000", "active", 2018),
 				new Employee("004", "Suresh",	24, "Male", 	"Infrastructure",	 	"25000", "active", 2010),
 				new Employee("006", "Sumit", 	19, "Male", 	"Account & Finance", 	"18000", "active", 2019),
 				new Employee("007", "Naveen", 	18, "Male", 	"Product Development", 	"17500", "active", 2018),
-				new Employee("008", "Abhishek", 18, "Male", 	"Product Development", 	"14000", "active", 2020),
+				new Employee("008", "Abhishek", 28, "Male", 	"Product Development", 	"14000", "active", 2020),
 				new Employee("009", "Kiran", 	19, "Female", 	"Infrastructure", 		"16000", "active", 2014),
 				new Employee("002", "Kanak", 	25, "Female", 	"Sales & Mkt",			"20000", "active", 2014)
 		);
-		
+		List<Integer> num = Arrays.asList(1,2,3,4,4,5);
 		//1. Gender count
 		Map<String, Long> genderCount = employees.stream().collect(Collectors.groupingBy(Employee::getGender, Collectors.counting()));
 		System.out.println(genderCount);
@@ -81,18 +84,61 @@ public class StreamExerciseJavaTechies {
 		
 		Optional<Employee> sorted = employees.stream().sorted(Comparator.comparingInt(Employee::getYearOfJoining)).findFirst();
 		System.out.println(sorted.get());
-		//10. How many male and female emp in sales and marketing department
+		//10. How many male and female emp in sales and marketing department //
 		Map<String, Long> saleMktEmpCount = employees.stream().filter(emp -> emp.getDept().equals("Sales & Mkt")).collect(Collectors.groupingBy(Employee::getGender, Collectors.counting()));
 		System.out.println("saleMktEmpCount : "+saleMktEmpCount);
 		saleMktEmpCount.entrySet().forEach(res -> System.out.println(res.getKey()+":"+res.getValue()));
+		System.out.println("--------------------------");
 		
 		//11. Average sal of male and female emp
+		Map<String, Double> genderAvgSal = employees.stream().collect(Collectors.groupingBy(Employee::getGender, Collectors.averagingInt(emp -> Integer.parseInt(emp.getSal()))));
+		genderAvgSal.entrySet().forEach(e -> System.out.println(e.getKey() +":" +e.getValue()));
+		System.out.println("--------------------------");
 		
 		//12. List down name of all emp in each dept
+		Map<String, List<Employee>> empAllDept = employees.stream().collect(Collectors.groupingBy(Employee::getDept, Collectors.toList()));
+		Set<Entry<String, List<Employee>>> entrySet = empAllDept.entrySet();
+		for (Entry<String, List<Employee>> entry : entrySet) {
+			System.out.print(entry.getKey() +" : ");
+			List<Employee> value = entry.getValue();
+			for (Employee emp : value) {
+				System.out.print(emp.getName()+", ");
+			}
+			System.out.println();
+		}
+		System.out.println("------");
+		empAllDept.entrySet().forEach(emp -> System.out.println(emp.getKey() + ":" + emp.getValue()   ));
+		System.out.println("--------------------------");
 		
-		//13. What is the average and total sal of whole organization 
+		//13. What is the average and total sal of whole organization
 		
-		//14. 
+		IntSummaryStatistics totalSumNdAverage = employees.stream().collect(Collectors.summarizingInt(emp -> Integer.parseInt(emp.getSal())));
+		System.out.println(totalSumNdAverage);
+		
+		IntSummaryStatistics calcInt = num.stream().collect(Collectors.summarizingInt(i -> i));
+		System.out.println("Sum:"+calcInt.getSum() +" Average:"+calcInt.getAverage());
+		System.out.println("--------------------------");
+		
+		//14. Separate the employee who are younger or equal to 25 year from those emp who are older than 25 year.
+		Map<Boolean, List<Employee>> partition = employees.stream().collect(Collectors.partitioningBy(e -> e.getAge() > 25));
+		Set<Entry<Boolean, List<Employee>>> entrySet2 = partition.entrySet();
+		for (Entry<Boolean, List<Employee>> entry : entrySet2) {
+			System.out.print(entry.getKey() +" : ");
+			List<Employee> value = entry.getValue();
+			for (Employee emp : value) {
+				System.out.print(emp.getName()+", ");
+			}
+			System.out.println();
+		}
+		System.out.println("--------------------------");
+		
+		
+		
+		//15. Who is the oldest emp in org? What is his age and dept?
+		Optional<Employee> empMaxAgeOp = employees.stream().collect(Collectors.maxBy(Comparator.comparingInt(Employee::getAge)));
+		System.out.println(empMaxAgeOp.get());
+		
+		Collectors.toSet(); // to get set from list
 		
 	}
 
---
+++ corejava-lambda/src/com/core/lambda/exericse/stream/WordFrequencyCalculate.java
@@ -0,0 +1,29 @@
+package com.core.lambda.exericse.stream;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class WordFrequencyCalculate {
+
+	public static void main(String[] args) {
+		List<String> cityList = Arrays.asList("Mumbai","Chennai","Mumbai","Chenai","Bangalore","Bangalore","Bangalore","Chennai","Bangalore");
+		Set<String> citySet= new HashSet(cityList);
+		for (String cityUnique : citySet) {
+			if(cityList.contains(cityUnique)) {
+				System.out.println(cityUnique +" " +Collections.frequency(cityList, cityUnique));
+			}
+		}
+		
+	  //Map<String, Long> collect = cityList.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+		Map<String, Long> collect = cityList.stream().collect(Collectors.groupingBy(str -> str, Collectors.counting()));
+		System.out.println(collect);
+		
+	}
+
+}
