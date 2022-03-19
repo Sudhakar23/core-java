a833a8f5b13f37aaec632fa3e45440302037b586
Sudhakar23 <sudhakar.prsd23@gmail.com>
2022-03-19 13:11:45 +0530

    arrays example added

---
+++ corejava-datastructure/src/com/datastructure/array/MergeArray.java
@@ -0,0 +1,27 @@
+package com.datastructure.array;
+
+import java.util.Arrays;
+
+public class MergeArray {
+
+	public static void main(String[] args) {
+//		Output: arr3[] = {1, 2, 3, 4, 4, 5, 6, 8}
+		int arr1[] = { 1, 3, 4, 5}, arr2[] = {2, 4, 6, 8}; 
+		int arr3[] = new int[arr1.length + arr2.length];
+		
+		for (int i = 0; i < arr3.length; ) {
+		   for(int j = 0; j<arr1.length; j++) {
+			   arr3[i] = arr1[j];
+			   i++;
+		   }
+		   for(int j = 0; j<arr2.length; j++) {
+			   arr3[i] = arr2[j];
+			   i++;
+		   }
+		   
+		}
+		Arrays.sort(arr3);
+		
+		System.out.println(arr3.toString());
+	}
+}
