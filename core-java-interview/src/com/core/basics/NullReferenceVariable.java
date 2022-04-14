01d77945e1e25a02f2d533b9b9f02d78cf728c7d
Sudhakar23 <sudhakar.prsd23@gmail.com>
2022-04-14 23:13:39 +0530

    new core file moved to corejava-core project

--- core-java-interview/src/com/core/basics/NullReferenceVariable.java
+++
@@ -1,9 +0,0 @@
-package com.core.basics;
-
-public class NullReferenceVariable {
-
-	private static Object obj;
-	public static void main(String[] args) {
-		System.out.println("Value of Object: "+ obj);
-	}
-}
---
+++ corejava-collection/src/com/java/collection/core/ImmutableListEample.java
@@ -0,0 +1,10 @@
+package com.java.collection.core;
+
+public class ImmutableListEample {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
