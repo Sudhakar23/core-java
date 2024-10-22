79ed446230d904f1c291955e6f076966e8b16335
Sudhakar23 <sudhakar.prsd23@gmail.com>
2024-10-22 09:08:04 +0530

    Deleted duplicate files

--- JavaCore/src/com/test/CoreTest.java
+++ JavaCore/src/com/test/CoreTest.java
@@ -3,19 +3,16 @@ package com.test;
 //Getting next heighest
 public class CoreTest {
 	public static void main(String[] args) {
-		
-		Integer arr[] = {2,3,6,7,1,3,3,2,3,4,10,6,2,10,2,3};
+
+		Integer arr[] = { 2, 3, 6, 7, 1, 3, 3, 2, 3, 4, 10, 6, 2, 10, 2, 3 };
 		int a = 0, b = 0;
-		
+
 		for (Integer letterCount : arr) {
-			
-			if(letterCount > a) {
+			if (letterCount > a) {
 				b = a;
-				
 				a = letterCount;
 			}
-			
-			if(letterCount < a && letterCount > b) {
+			if (letterCount < a && letterCount > b) {
 				b = letterCount;
 			}
 		}
